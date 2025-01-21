e3554eab45429b01c71d6e41a5ac1f05e81974e0
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-01-21 13:58:16 -0300

    Conclusao capitulo 2 do curso de python fornecido pela huawei

--- Exemplo.py
+++ Exemplo.py
@@ -1,5 +1,41 @@
+#exemplo 1
+print("--------------Teste de objetos Python----------------------")
 quantidade = 3
 precouni = 36.2
 total = quantidade * precouni
 msg = 'Total ='
 print(msg, "{:.2f}".format(total))
+
+
+#exemplo 2: Operadores Lógicos
+print("-------------------Operadores Logicos------------------")
+
+A = 14
+B = 5
+C = A + B
+print("Soma:", C)
+C = A - B
+print("Subtracao:", C)
+C = A * B
+print("Multiplicacao:", C)
+C = A / B
+print("Divisao:", C)
+C = A // B
+print("Divisao inteira:", C)
+C = A % B
+print("Resto da divisão(modulo):", C)
+C = -A
+print("Menos unário de A:", C)
+C = -B
+print("Menos unário de B:", C)
+C = A ** B
+print("Potenciacao:", C)
+
+#exemplo 3: Biblioteca math
+#funcao sqrt que calcula raiz quadrada
+
+from math import sqrt
+print("------------------------Teste biblioteca math-------------------")
+X = 49
+R = sqrt(X)
+print("A raiz quadrada e:",R)
--- Exercicio-1.py
+++ Exercicio-1.py
@@ -1,1 +1,24 @@
-print('hello world!!!')
+print('Execucção de Expresões Aritméticas com Multiplos Operandos')
+A = 10
+B = 25
+C = 7
+
+R = 2 * A + B
+print("Resultado da operacao:", R)
+
+R = 2 * (A + B)
+print("Resultado da operacao:", R)
+
+X = (A + B) / (A + C)
+print("Resultado da operacao:", X)
+
+X = (2 * B - C) / (2 * A)
+print("Resultado da operacao:", X)
+
+X = -A * 2 + B
+print("Resultado da operacao:", X)
+
+X = A % B * C
+print("Resultado da operacao:", X)
+
+
