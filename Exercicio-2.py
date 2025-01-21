f60a983786a19fbfb98daf74c0165605ffa63adb
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-01-21 15:59:51 -0300

    Inclusão da lista de exercicios fornecida no capitulo 3

---
+++ Exercicio-2.py
@@ -0,0 +1,4 @@
+# Escreva um programa que leia os nomes de três pessoas de uma família: mãe, pai e criança.
+# O programa deve exibir na tela a mensagem.
+# "Os adultos {mãe} e {pai} são os responsáveis por {criança}"
+# Faça de dois modos: com o método .format() e com f-string
---
+++ Exercicio-3.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um texto e mostre na tela o texto e a quantidade de
+# caracteres que ele contém, usando a seguinte mensagem:
+# "O texto {AquiColoqueOTexto} contém {Quantidade} caracteres"
+# Faça de dois modos: com o método .format() e com f-string
+
+# DICA: Use a função len()
+
