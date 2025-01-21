a1c37efc642f9af18dcbf0f5e09d1ce1ad650150
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-01-21 16:49:01 -0300

    Inclusão da lista de exercicios fornecida no capitulo 3

--- Exercicio-2.py
+++ Exercicio-2.py
@@ -1,4 +1,15 @@
 # Escreva um programa que leia os nomes de três pessoas de uma família: mãe, pai e criança.
 # O programa deve exibir na tela a mensagem.
 # "Os adultos {mãe} e {pai} são os responsáveis por {criança}"
-# Faça de dois modos: com o método .format() e com f-string
+# Faça de dois modos: com o metodo .format() e com f-string
+
+# Objetos
+mae = input('Digite o nome da mae: ')
+pai = input('Digite o nome do pai: ')
+crianca = input('Digite o nome da crianca: ')
+
+# Metodo .format()
+print("Os adultos {0} e {1} sao os responsaveis por {2}".format(mae, pai, crianca))
+
+# Metodo f-string
+print(f"Os adultos {mae} e {pai} sao os responsaveis por {crianca}")
--- Exercicio-3.py
+++ Exercicio-3.py
@@ -1,7 +1,15 @@
 # Escreva um programa que leia um texto e mostre na tela o texto e a quantidade de
 # caracteres que ele contém, usando a seguinte mensagem:
 # "O texto {AquiColoqueOTexto} contém {Quantidade} caracteres"
-# Faça de dois modos: com o método .format() e com f-string
-
+# Faça de dois modos: com o metodo .format() e com f-string
 # DICA: Use a função len()
 
+#Objetos
+texto = input("Digite um texto: ")
+qtde = len(texto)
+
+#Metodo .format()
+print("O texto {0} contem {1} caracteres".format(texto, qtde))
+
+#Metodo f-string
+print(f"O texto {texto} contem {qtde} caracteres")
