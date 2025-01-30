75076d2e02c1b8eabc7a7d5ac82dfdadcdb28def
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-01-30 11:03:30 -0300

    Inclusão da lista de exercicios fornecida no capitulo 4

--- Exercicio-4.10.py
+++ Exercicio-4.10.py
@@ -3,3 +3,31 @@
 # Regra: Para três números formarem um triângulo precisa ocorrer que:
 # a) os três números precisam ser maiores que zero;
 # b) a soma dos dois menores valores deve ser maior que o terceiro.
+
+a = int(input('Digite um numero inteiro: '))
+b = int(input('Digite um numero inteiro: '))
+c = int(input('Digite um numero inteiro: '))
+
+maior = a
+if b > maior:
+    maior = b
+if c > maior:
+    maior = c
+
+somamenores = 0
+if a != maior:
+    somamenores += a
+if b != maior:
+    somamenores += b
+if c != maior:
+    somamenores += c
+
+if a > 0 and b > 0 and c > 0 and somamenores > maior:
+    if a == b == c:
+        print('triangulo Equilatero')
+    elif a == b != c or a !=b == c or a == c != b:
+        print('Triangulo isosceles')
+    else:
+        print('Triangulo escaleno')
+else:
+    print(f'nao foi possivel formar um triangulo com os numeros {a}, {b} e {c}')
--- Exercicio-4.9.py
+++ Exercicio-4.9.py
@@ -2,3 +2,14 @@
 # a) "Os três valores são iguais"
 # b) "Há dois valores iguais e um diferente"
 # c) "Os três valores são diferentes"
+
+a = int(input('Digite um numero inteiro: '))
+b = int(input('Digite um numero inteiro: '))
+c = int(input('Digite um numero inteiro: '))
+
+if a == b == c:
+    print('Os tres valores são iguais')
+elif a == b or a == c or b == c:
+    print('Há dois valores iguais e um diferente')
+else:
+    print('Os três valores são diferentes')
